4f458fb65583c97d7e61191baa7a896fffc47900
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 15:04:27 +0000

    created seeds file for sample data fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,14 @@ ActiveRecord::Schema.define(version: 20170621151151) do
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
-    t.string   "tittle"
+    t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "portfolios", force: :cascade do |t|
-    t.string   "tittle"
+    t.string   "title"
     t.string   "subtitle"
     t.text     "body"
     t.text     "main_image"

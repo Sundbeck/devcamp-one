329c26083d24a24d4baf0f6f804756ebe7a3609d
Jordan Hudgens <jordan@devcamp.com>
2017-07-19 22:18:03 +0000

    Refactored before action for portfolio setting, fixed css issue with nav bar links

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout "portfolio"
   def index
     @portfolio_items = Portfolio.all
@@ -26,13 +27,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
-     
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-  
 
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -44,13 +41,9 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-    # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
@@ -59,7 +52,9 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
   private
   
   def portfolio_params
